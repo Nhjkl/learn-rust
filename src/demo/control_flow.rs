55d0601daf34445cb24d0c4a907c87583e4d2bc4
liuxian <liuxiangogo@gmail.com>
2024-04-09 13:33:05 +0800

    learn: control flow

--- src/demo/control_flow.rs
+++ src/demo/control_flow.rs
@@ -1,9 +1,14 @@
+use colored::Colorize;
+
 pub fn run() {
     println!("Hello, world! control flow");
     if_example();
     let_example();
     // loop_example();
     break_example();
+    while_example();
+    for_example();
+    for_range();
 }
 
 /// NOTE: in rust, all control flow is 'if'
@@ -33,7 +38,7 @@ fn loop_example() {
 
 /// NOTE: break
 fn break_example() {
-    println!("Let's break loop example-----------------");
+    println!("{}", "Let's break loop example-----------------".green());
     let mut counter = 0;
     let result = loop {
         counter += 1;
@@ -46,10 +51,32 @@ fn break_example() {
 
 /// NOTE: while 条件循环
 fn while_example() {
+    println!("{}", "Let's while loop example-----------------".green());
     let mut number = 3;
     while number != 0 {
         println!("{}!", number);
-        mumber = 1;
+        number -= 1;
     }
     println!("LIFTOFF!!!");
+}
+
+/// NOTE: 使用for循环，遍历集合
+fn for_example() {
+    println!("{}", "Let's for loop example-----------------".green());
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+}
+
+/// NOTE: 使用Range，for循环
+fn for_range() {
+    println!(
+        "{}",
+        "Let's for_range loop example-----------------".green()
+    );
+    for number in (1..4).rev() {
+        // NOTE: 这里是从 1 到 4，但是不包括 4, rev() 是倒序
+        println!("{}!", number);
+    }
 }
