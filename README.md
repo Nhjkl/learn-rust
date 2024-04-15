99c5240d2cd6d05ef6f3275c0811a158f8808b32
liuxian <liuxiangogo@gmail.com>
2024-04-15 08:33:44 +0800

    learn: 将单线程服务器变为多线程服务器, 停机与清理

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Rust 官方文档学习，笔记，代码练习
+
+[程序设计语言](https://rustwiki.org/zh-CN/book/title-page.html)
+
+[配套视频-杨旭](https://www.bilibili.com/video/BV1hp4y1k7SV?p=1&vd_source=afb7e391b368ef3b185a3ef1edf10dda)
+
+[The Rust Programming Language](https://doc.rust-lang.org/book/title-page.html)
+
 ```bash
 RUST_BACKTRACE=1 cargo run --bin demo
 CASE_INSENSITIVE=1 cargo run --bin minigrep ho hello.txt  >> output.txt
--- src/webserver.rs
+++ src/webserver.rs
@@ -1,54 +1,50 @@
 use std::{
     fs,
-    io::{self, Read, Write},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use learning_rust::tpool::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
+    // for stream in listener.incoming().take(2) {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
-        handle_connection(stream).unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
-fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    Result::unwrap(stream.read(&mut buffer));
-
-    // NOTE: 请求格式
-    // Method Request-URI HTTP-Version CRLF
-    // headers CRLF
-    // message-body
-
-    // NOTE: 响应格式
-    // HTTP-Version Status-Code Reason-Phrase CRLF
-    // headers CRLF
-    // message-body
+    let _ = stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, file_name) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
-    let contents = fs::read_to_string(file_name)?;
+    let contents = fs::read_to_string(filename).unwrap();
 
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
+    let response = format!("{}{}", status_line, contents);
 
-    let _write = stream.write(response.as_bytes())?;
-
-    stream.flush()?;
-
-    Ok(())
+    let _ = stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
